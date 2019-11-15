4ae5a3df73a214b9f31b264899e24662920f9379
lorenanicole <me@lorenamesa.com>
2019-11-15 13:53:06 +0000

    Add MeetUp scraper script that allows for ratelimiting

---
+++ scripts/meetup-scraper/pyladies-meetup-scraper.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+"""
+script to generate PyLadies meetup .csv with the following information:
+- meetup name
+- meetup id
+- meetup url
+- date of last meetup event
+- meetup organizers
+
+To use you'll need to register a MeetUp app at https://secure.meetup.com/meetup_api/oauth_consumers/ and insert the following values in the `.env` file:
+- GITHUB_TOKEN
+- MEETUP_CLIENT_ID
+- MEETUP_CLIENT_SECRET
+- MEETUP_REDIRECT_URI
+
+You can run from the commandline with: pyladies-meetup-scraper.py
+"""
+import base64
+import time
+
+from dotenv import load_dotenv
+from functools import wraps
+from os import getenv
+from os.path import join, dirname
+from pathlib import Path
+from urllib.parse import urlencode
+import requests
+import yaml
+
+
+__author__ = "Lorena Mesa"
+__email__ = "lorena@pyladies.com"
+
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+CURRENT_DIR = Path(__file__).parent.resolve()
+
+# Setup env tokens
+MEETUP_CLIENT_ID = getenv('MEETUP_CLIENT_ID')
+MEETUP_CLIENT_SECRET = getenv('MEETUP_CLIENT_SECRET')
+MEETUP_REDIRECT_URI = getenv('MEETUP_REDIRECT_URI')
+GITHUB_TOKEN = getenv('GITHUB_TOKEN')
+
+# Ratelimiting defaults
+MAX_MEETUP_REQUESTS_PER_HOUR = 200
+
+
+def ratelimit(number_times):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            nonlocal number_times
+
+            completed, response = fn(*args,**kwargs)
+            number_times = response.headers.get('X-RateLimit-Remaining')
+            if not completed and response.status_code == 429:
+                seconds_until_reset = int(meetup_resp.headers.get('X-RateLimit-Reset'))
+                if seconds_until_reset and meetup_resp.json().get('errors')[0].get('code') == 'throttled':
+                    print(f'Sleeping {seconds_until_reset} until throttle is complete ...')
+                    time.sleep(seconds_until_reset)
+                    print(f'Trying again now that throttle is complete ...')
+                    completed, response = fn(*args, **kwargs)
+                return completed, response
+
+            return completed, response
+
+        return wrapper
+    return decorator
+
+
+class MeetUpApi(object):
+    """
+    Lightweight wrapper to make requests against MeetUp API.
+    """
+
+    api_url = 'https://api.meetup.com'
+    api_auth_url = 'https://secure.meetup.com'
+
+    def __init__(self, client_id, client_secret, redirect_uri):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.redirect_uri = redirect_uri
+        self.token = None
+
+    def get_bearer_token(self):
+        auth_endpoint = f'{MeetUpApi.api_auth_url}/oauth2/authorize?client_id={self.client_id}&redirect_uri={self.redirect_uri}&response_type=code'
+        code = input(f'Visit {auth_endpoint} and enter response code:')
+
+        data = {
+            'client_id': f'{self.client_id}',
+            'client_secret':  f'{self.client_secret}',
+            'redirect_uri': f'{self.redirect_uri}',
+            'code': f'{code}',
+            'grant_type': 'authorization_code'
+        }
+
+        try:
+            response = requests.post(
+                f'{MeetUpApi.api_auth_url}/oauth2/access',
+                data=data
+            )
+            if response.ok:
+                data = response.json()
+                self.token = data.get('access_token')
+                self.refresh_token = data.get('refresh_token')
+                print(response.status_code, self.token, self.refresh_token)
+                return True
+            return False
+        except Exception as e:
+            raise Exception('Unable to obtain MeetUp access token.')
+
+    @ratelimit(number_times=MAX_MEETUP_REQUESTS_PER_HOUR)
+    def get_groups(self, topic_ids):
+        """
+        :param  meetup_ids: list of numeric topic_ids, if unknown use the get_group endpoint
+        :return list of pyladies meetup dicts
+        """
+
+        query_params = {
+            'category': topic_ids,
+        }
+
+        query_string = urlencode(query_params)
+
+        find_groups_endpoint = u'{}/{}?{}'.format(
+            MeetUpApi.api_url, 'find', 'groups', query_string
+        )
+
+        try:
+            response = requests.get(
+                find_groups_endpoint,
+                headers={'Authorization': f'Bearer {self.token}'}
+            )
+            if response.ok:
+                return True, response
+            return False, response
+        except Exception as e:
+            raise Exception('Exception processing MeetUp data')
+
+    @ratelimit(number_times=MAX_MEETUP_REQUESTS_PER_HOUR)
+    def get_group(self, chapter_name):
+        """
+        :param  chapter_name: meetup chapter urlnames e.g. meetup.com/Chicago-Pyladies/ the urlname is 'Chicago-Pyladies'
+        :return dict of pyladies meetup group info
+        """
+        group_endpoint = f'{MeetUpApi.api_url}/{chapter_name}'
+
+        try:
+            response = requests.get(
+                group_endpoint,
+                headers={'Authorization': f'Bearer {self.token}'}
+            )
+            if response.ok:
+                return True, response
+            return False, response
+        except Exception as e:
+            raise Exception('Exception processing MeetUp data')
+
+def download_pyladies_chapters(token):
+    headers = {'Authorization': f'token {token}'}
+    response = requests.get(
+        url='https://api.github.com/repos/pyladies/pyladies/contents/www/config.yml',
+        headers=headers
+    )
+
+    content = response.json().get('content')
+    chapters = str(base64.b64decode(content), 'utf-8')
+
+    chapters_file = f'{CURRENT_DIR}/pyladies_meetup_chapters.yml'
+    with open(chapters_file, 'w+') as f:
+        f.write(chapters)
+
+    return chapters_file
+
+if __name__ == "__main__":
+    if not GITHUB_TOKEN:
+        GITHUB_TOKEN = input('To download the PyLadies MeetUp chapters, please provide a GitHub token with repo access privileges.\n')
+    print('Download starting!')
+
+    chapter_file = download_pyladies_chapters(GITHUB_TOKEN)
+
+    if not chapter_file:
+        exit(-1)
+
+    with open(chapter_file, 'r') as stream:
+        chapter_data = yaml.safe_load(stream)
+
+    meetup_api = MeetUpApi(client_id=MEETUP_CLIENT_ID, client_secret=MEETUP_CLIENT_SECRET, redirect_uri=MEETUP_REDIRECT_URI)
+    token_obtained = meetup_api.get_bearer_token()
+    if not token_obtained:
+        print('Unable to obtain token, exiting ...')
+        exit(-1)
+
+    for chapter in chapter_data.get('chapters'):
+        meetup_url_name = chapter.get('meetup')
+        if not meetup_url_name:
+            continue
+
+        completed, meetup_resp = meetup_api.get_group(meetup_url_name)
+
+        if meetup_resp.status_code == 404:
+            print(f'Chapter {meetup_url_name} has no meetup, deleting info')
+            chapter.pop('meetup', None)
+            continue
+
+        if not completed:
+            print(f'Error {meetup_resp.status_code}, skipping chapter {meetup_url_name}')
+            continue
+
+        meetup_resp = meetup_resp.json()
+
+        if chapter.get('meetup_id') != meetup_resp.get('id'):
+            print(f"MeetUp Chapter {chapter.get('name')} id is wrong {meetup_resp.get('id')}")
+            chapter['meetup_id'] = int(meetup_resp.get('id'))
+
+        if meetup_resp.get('organizer'):
+            chapter['organizer'] = meetup_resp.get('organizer').get('name')
+
+        chapter['location'] = {
+            'latitude': meetup_resp.get('lat'),
+            'longitude': meetup_resp.get('lon')
+        }
+
+        if meetup_resp.get('group_photo'):
+            chapter['meetup_image'] = meetup_resp.get('group_photo').get('highres_link')
+
+        if meetup_resp.get('pro_network'):
+            chapter['pro_network'] = meetup_resp.get('pro_network').get('name')
+
+    with open(chapter_file, 'w+') as stream:  # Overwrites original file
+        chapter_data = yaml.dump(chapter_data, stream)
+
+    print('Done!')
