404d3700c2b94ccd807913506ccb0ff87d7bc0b0
lorenanicole <me@lorenamesa.com>
2019-11-18 13:31:39 -0600

    Make MeetUp script flake8 compliant

--- scripts/meetup-scraper/pyladies-meetup-scraper.py
+++ scripts/meetup-scraper/pyladies-meetup-scraper.py
@@ -2,14 +2,15 @@
 #  -*- coding: utf-8 -*-
 
 """
-script to generate PyLadies meetup .csv with the following information:
-- meetup name
-- meetup id
-- meetup url
-- date of last meetup event
-- meetup organizers
+Script to download PyLadies config.yaml and update the following
+MeetUp information, if a chapter has a MeetUp:
+- MeetUp name
+- MeetUp id
+- MeetUp organizers
 
-To use you'll need to register a MeetUp app at https://secure.meetup.com/meetup_api/oauth_consumers/ and insert the following values in the `.env` file:
+To use you'll need to register a MeetUp app at
+https://secure.meetup.com/meetup_api/oauth_consumers/ and insert the
+following values in the `.env` file:
 - GITHUB_TOKEN
 - MEETUP_CLIENT_ID
 - MEETUP_CLIENT_SECRET
@@ -55,14 +56,20 @@ def ratelimit(number_times):
         def wrapper(*args, **kwargs):
             nonlocal number_times
 
-            completed, response = fn(*args,**kwargs)
+            completed, response = fn(*args, **kwargs)
             number_times = response.headers.get('X-RateLimit-Remaining')
             if not completed and response.status_code == 429:
-                seconds_until_reset = int(meetup_resp.headers.get('X-RateLimit-Reset'))
-                if seconds_until_reset and meetup_resp.json().get('errors')[0].get('code') == 'throttled':
-                    print(f'Sleeping {seconds_until_reset} until throttle is complete ...')
+                seconds_until_reset = int(
+                    meetup_resp.headers.get('X-RateLimit-Reset')
+                )
+
+                meetup_errors = meetup_resp.json().get('errors')
+                meetup_errors = [e.get('code') for e in meetup_errors]
+
+                if seconds_until_reset and 'throttled' in meetup_errors:
+                    print(f'Throttling {seconds_until_reset} ...')
                     time.sleep(seconds_until_reset)
-                    print(f'Trying again now that throttle is complete ...')
+                    print(f'Trying again ...')
                     completed, response = fn(*args, **kwargs)
                 return completed, response
 
@@ -87,7 +94,10 @@ class MeetUpApi(object):
         self.token = None
 
     def get_bearer_token(self):
-        auth_endpoint = f'{MeetUpApi.api_auth_url}/oauth2/authorize?client_id={self.client_id}&redirect_uri={self.redirect_uri}&response_type=code'
+        auth_endpoint = f'{MeetUpApi.api_auth_url}/oauth2/authorize' \
+                        f'?client_id={self.client_id}' \
+                        f'&redirect_uri={self.redirect_uri}' \
+                        f'&response_type=code'
         code = input(f'Visit {auth_endpoint} and enter response code:')
 
         data = {
@@ -111,12 +121,12 @@ class MeetUpApi(object):
                 return True
             return False
         except Exception as e:
-            raise Exception('Unable to obtain MeetUp access token.')
+            raise e
 
     @ratelimit(number_times=MAX_MEETUP_REQUESTS_PER_HOUR)
     def get_groups(self, topic_ids):
         """
-        :param  meetup_ids: list of numeric topic_ids, if unknown use the get_group endpoint
+        :param  meetup_ids: list of numeric topic_ids
         :return list of pyladies meetup dicts
         """
 
@@ -139,12 +149,13 @@ class MeetUpApi(object):
                 return True, response
             return False, response
         except Exception as e:
-            raise Exception('Exception processing MeetUp data')
+            raise e
 
     @ratelimit(number_times=MAX_MEETUP_REQUESTS_PER_HOUR)
     def get_group(self, chapter_name):
         """
-        :param  chapter_name: meetup chapter urlnames e.g. meetup.com/Chicago-Pyladies/ the urlname is 'Chicago-Pyladies'
+        :param  chapter_name: meetup chapter urlnames
+        e.g. meetup.com/Chicago-Pyladies/ the urlname is 'Chicago-Pyladies'
         :return dict of pyladies meetup group info
         """
         group_endpoint = f'{MeetUpApi.api_url}/{chapter_name}'
@@ -158,12 +169,14 @@ class MeetUpApi(object):
                 return True, response
             return False, response
         except Exception as e:
-            raise Exception('Exception processing MeetUp data')
+            raise e
+
 
 def download_pyladies_chapters(token):
     headers = {'Authorization': f'token {token}'}
     response = requests.get(
-        url='https://api.github.com/repos/pyladies/pyladies/contents/www/config.yml',
+        url='https://api.github.com/repos/pyladies/'
+            'pyladies/contents/www/config.yml',
         headers=headers
     )
 
@@ -176,9 +189,12 @@ def download_pyladies_chapters(token):
 
     return chapters_file
 
+
 if __name__ == "__main__":
     if not GITHUB_TOKEN:
-        GITHUB_TOKEN = input('To download the PyLadies MeetUp chapters, please provide a GitHub token with repo access privileges.\n')
+        GITHUB_TOKEN = input('To download the PyLadies MeetUp chapters, '
+                             'please provide a GitHub token with repo '
+                             'access privileges.\n')
     print('Download starting!')
 
     chapter_file = download_pyladies_chapters(GITHUB_TOKEN)
@@ -189,7 +205,11 @@ if __name__ == "__main__":
     with open(chapter_file, 'r') as stream:
         chapter_data = yaml.safe_load(stream)
 
-    meetup_api = MeetUpApi(client_id=MEETUP_CLIENT_ID, client_secret=MEETUP_CLIENT_SECRET, redirect_uri=MEETUP_REDIRECT_URI)
+    meetup_api = MeetUpApi(
+        client_id=MEETUP_CLIENT_ID,
+        client_secret=MEETUP_CLIENT_SECRET,
+        redirect_uri=MEETUP_REDIRECT_URI
+    )
     token_obtained = meetup_api.get_bearer_token()
     if not token_obtained:
         print('Unable to obtain token, exiting ...')
@@ -208,13 +228,15 @@ if __name__ == "__main__":
             continue
 
         if not completed:
-            print(f'Error {meetup_resp.status_code}, skipping chapter {meetup_url_name}')
+            print(f'Error {meetup_resp.status_code}, '
+                  f'skipping chapter {meetup_url_name}')
             continue
 
         meetup_resp = meetup_resp.json()
 
         if chapter.get('meetup_id') != meetup_resp.get('id'):
-            print(f"MeetUp Chapter {chapter.get('name')} id is wrong {meetup_resp.get('id')}")
+            print(f'MeetUp Chapter {chapter.get("name")} '
+                  f'id is wrong {meetup_resp.get("id")}')
             chapter['meetup_id'] = int(meetup_resp.get('id'))
 
         if meetup_resp.get('organizer'):
@@ -226,7 +248,8 @@ if __name__ == "__main__":
         }
 
         if meetup_resp.get('group_photo'):
-            chapter['meetup_image'] = meetup_resp.get('group_photo').get('highres_link')
+            chapter['meetup_image'] = meetup_resp\
+                .get('group_photo').get('highres_link')
 
         if meetup_resp.get('pro_network'):
             chapter['pro_network'] = meetup_resp.get('pro_network').get('name')
